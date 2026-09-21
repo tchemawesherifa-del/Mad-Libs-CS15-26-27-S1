2ac80cffa1eb9b00e2e10ca095c3a0687964feeb
tchemawesherifa-del <tchemawesherifa@gmail.com>
2026-09-21 14:21:56 -0600

    Submitting Assignment

---
+++ .devcontainer/main.py
@@ -0,0 +1,16 @@
+noun = input("Noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb ending in -ing\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun = input("Plural noun\n")
+adjective_2 = input("Another adjective\n")
+
+value= 3
+
+#my_string = 'I have' + str(value) + 'apple'
+
+my_string = 'the number of aples I have is '.
+my_string = f 'I have (value) apples!'
+
+
